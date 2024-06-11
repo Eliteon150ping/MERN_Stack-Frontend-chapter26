97eb60a1a9008956b5401df2be3f3b4f8da0ed04
Rishen <rishensurjoobhalee@example.com>
2024-06-11 11:48:24 +0200

    frontend/chapter-26

---
+++ readme.txt
@@ -0,0 +1,7 @@
+echo "# MERN_Stack-Frontend-chapter26" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Eliteon150ping/MERN_Stack-Frontend-chapter26.git
+git push -u origin main
---
+++ src/services/movies.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+class MovieDataService {
+  getAll(page = 0) {
+    return axios.get(`https://mern-stack-backend-hoh8.onrender.com/api/v1/movies?page=${page}`);
+  }
+  get(id) {
+    return axios.get(`https://mern-stack-backend-hoh8.onrender.com/api/v1/movies/id/${id}`);
+  }
+  find(query, by = "title", page = 0) {
+    return axios.get(
+      `https://mern-stack-backend-hoh8.onrender.com/api/v1/movies?${by}=${query}&page=${page}`
+    );
+  }
+  createReview(data) {
+    return axios.post("https://mern-stack-backend-hoh8.onrender.com/api/v1/movies/review", data);
+  }
+  updateReview(data) {
+    return axios.put("https://mern-stack-backend-hoh8.onrender.com/api/v1/movies/review", data);
+  }
+  deleteReview(id, userId) {
+    return axios.delete("https://mern-stack-backend-hoh8.onrender.com/api/v1/movies/review", {
+      data: { review_id: id, user_id: userId },
+    });
+  }
+  getRatings() {
+    return axios.get("https://mern-stack-backend-hoh8.onrender.com/api/v1/movies/ratings");
+  }
+}
+export default new MovieDataService();
